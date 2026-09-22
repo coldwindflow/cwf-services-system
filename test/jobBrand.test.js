8b2a372025c8e7fd810a77487231fd8f1f1ae06f
coldwindflow <dtga2mkung@gmail.com>
2026-09-22 16:51:11 +0700

    Update Admin Add build pin for Issue 374 (jobBrand.test.js)

--- test/jobBrand.test.js
+++ test/jobBrand.test.js
@@ -98,7 +98,7 @@ test("admin/technician DTOs and UIs expose brand while assignment and collision 
 });
 
 test("technician cache/version markers match every changed shell reference", () => {
-  const build = "20260920_issue366_axs_pricing_v1";
+  const build = "20260921_issue374_single_tech_v1";
   assert.match(read("app.js"), new RegExp(build));
   assert.match(read("sw.js"), new RegExp(build));
   assert.match(read("cwf-pwa.js"), new RegExp(build));
